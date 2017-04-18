1da04de73398609f639d46aaaea2fa68dccf2da3
UserTundra <haizbq@gmail.com>
2017-04-18 23:03:20 +0400

    totally have no idea

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+    public void paintStones(MouseEvent e){
+        System.out.println("ZZZZ");
+        Image image = new Image("sample/images/our_stone.png");
+        ImageView view = (ImageView) e.getSource();
+        view.setImage(image);
+        System.out.println("it works");
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Калах");
         primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.setResizable(false);
         primaryStage.show();
         System.out.println();
     }
